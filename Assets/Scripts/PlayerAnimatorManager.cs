4d0fc131f8b9916e968cc90bde33541e321465c9
Chandler Lane <chandler.script@gmail.com>
2021-01-04 17:58:46 -0500

    Import Kyle and add PlayerAnimatorManager script

---
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ChandlerLane.Scripts
+{
+    public class PlayerAnimatorManager : MonoBehaviour
+    {
+        #region MonoBehaviour Callbacks
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        #endregion
+    }
+}
