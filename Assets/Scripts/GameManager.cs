f0dd51159edbc4c6d3f8ddb9205399f87e511322
Chandler Lane <chandler.script@gmail.com>
2021-01-04 14:23:08 -0500

    Load launcher screen on exit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,6 +17,18 @@ namespace ChandlerLane.Scripts
             SceneManager.LoadScene(0);
         }
 
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+                
+                LoadArena();
+            }
+        }
+
         #endregion
 
         #region Public Methods
@@ -28,6 +40,21 @@ namespace ChandlerLane.Scripts
 
         #endregion
 
+        #region Private Methods
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to load a level but we are not the master Client");
+            }
+            
+            Debug.LogFormat("PhotonNetwork : Loading level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+        }
+
+        #endregion
+        
     }
     
 }
