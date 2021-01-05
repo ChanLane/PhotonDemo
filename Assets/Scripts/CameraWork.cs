8a410ec8ebbf33b775117eada8bb411bd4a72d0f
Chandler Lane <chandler.script@gmail.com>
2021-01-05 03:12:06 -0500

    Camera Settings

--- Assets/Scripts/CameraWork.cs
+++ Assets/Scripts/CameraWork.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using Sirenix.OdinInspector;
 
-public class CameraWork : MonoBehaviour
+namespace ChandlerLane.Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CameraWork : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        #region Private Fields
+
+        [BoxGroup("Camera Settings")]
+        [Tooltip("The distance in the local x-z plane to the target")] 
+        [SerializeField]
+        private float distance = 7.0f;
+
+        [BoxGroup("Camera Settings")]
+        [Tooltip("The height we want the camera to be above the target")] 
+        [SerializeField]
+        private float height = 7.0f;
+
+        [BoxGroup("Camera Settings")]
+        [Tooltip("Allow the camera to be offseted vertically from the target, for example giving more view of the scenery and less ground")]
+        [SerializeField]
+        private Vector3 centerOffset = Vector3.zero;
+
+        [BoxGroup("Camera Settings")]
+        [Tooltip("The Smoothing for the camera to follow the target")] 
+        [SerializeField]
+        private float SmoothSpeed = 0.125f;
+       
+        #endregion
         
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }    
 }
+
