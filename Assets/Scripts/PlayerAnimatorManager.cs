1acfda6e30216211fad081ef36960e8879046657
Chandler Lane <chandler.script@gmail.com>
2021-01-05 02:37:06 -0500

    Animated Kyle

--- Assets/Scripts/PlayerAnimatorManager.cs
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -2,22 +2,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 namespace ChandlerLane.Scripts
 {
     public class PlayerAnimatorManager : MonoBehaviour
     {
+
+        #region PrivateFields
+
+        private Animator _animator;
+        [SerializeField] private float directionDampTime = 0.25f;
+
+        #endregion
+        
+        
         #region MonoBehaviour Callbacks
 
+       
         private void Start()
         {
-            
+            _animator = GetComponent<Animator>();
+            if (!_animator)
+            {
+                Debug.LogError("PlayerAnimatorManager is Missing Animator Component", this);
+            }
         }
 
         private void Update()
         {
-            
+            if (!_animator)
+            {
+                return;
+            }
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            if (v < 0)
+            {
+                v = 0;
+            }
+            _animator.SetFloat("Speed", h * h + v * v);
+            _animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
         }
 
         #endregion
